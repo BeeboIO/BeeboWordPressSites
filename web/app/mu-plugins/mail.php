9f0f645df868d5e75a9275728ff4b6b0de7bdcd5
collegeman <aaron@collegeman.net>
2025-08-12 09:47:44 -0400

    Improve readability of this MU plugin

--- web/app/mu-plugins/mail.php
+++ web/app/mu-plugins/mail.php
@@ -1,13 +1,10 @@
 <?php
 
-// Looking to send emails in production? Check out our Email API/SMTP product!
-function mailtrap($phpmailer) {
-  $phpmailer->isSMTP();
-  $phpmailer->Host = SMTP_HOST;
-  $phpmailer->SMTPAuth = SMTP_AUTH;
-  $phpmailer->Port = SMTP_PORT;
-  $phpmailer->Username = SMTP_USERNAME;
-  $phpmailer->Password = SMTP_PASSWORD;
-}
-
-add_action('phpmailer_init', 'mailtrap');
+add_action('phpmailer_init', function ($phpmailer) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host = constant('SMTP_HOST');
+    $phpmailer->SMTPAuth = constant('SMTP_AUTH');
+    $phpmailer->Port = constant('SMTP_PORT');
+    $phpmailer->Username = constant('SMTP_USERNAME');
+    $phpmailer->Password = constant('SMTP_PASSWORD');
+});
